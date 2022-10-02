41b80f4206a251ed046367304c6baa4ac176fa0a
NiktoX2 <69085963+NiktoX2@users.noreply.github.com>
2022-10-02 21:20:21 +0300

    Test

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*
+* lang — Ru
+* author — NiktoX2
+* — https://vk.com/niktox2
+* team — Lost Summer Time
+* — https://vk.com/lost.summer.time
+*
+*
+*/
+
+window.ondragstart = () => { return false };
